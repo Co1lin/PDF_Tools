ccf3e8c396331cab081722a55bb4ea91aba1dba1
enjoycolin <hnpengjj@gmail.com>
2020-02-12 23:13:27 +0800

    init

---
+++ pdfmerger.py
@@ -0,0 +1,167 @@
+import os
+import re
+import time
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
+
+class PdfMerger(object):
+
+    def __init__(self):
+        print('Initializing...')
+        '''Set some constants according to your needs'''
+        # directory of PDFs need to be merged (DON'T end with "/")
+        self.DIR_PATH = '/Users/colin/Downloads/大学物理学'
+        # times that the match operation needs to be performed
+        # TIMES = 2
+        # the list of regular expressions
+        RE_LIST = [
+            '-{1,1}\d+',
+            '\d+'
+        ]
+        # the start index that can be matched of the PDF
+        self.START_INDEX = 1
+        # a value greater than the number of PDFs
+        self.INF_VALUE = 100000  # usually don't need to change unless you have toooo many PDFs
+        '''Values below are not supposed to be changed'''
+        # max of the index that can be matched of the PDF
+        self.max_index = -100000
+        # number of invalid files
+        self.invalid = 0
+        # number of valid files
+        self.valid = 0
+        #
+        self.merged = 0
+        # patterns that will be generated according to RE_LIST
+        self.PATTERNS = []
+        # compile regular expressions to patterns
+        for regexp in RE_LIST:
+            self.PATTERNS.append(re.compile(regexp))
+        # dictionary for iterate
+        self.map = {}
+
+    def __get_index(self, name):
+        original_name = name
+        # perform the regular expression matching sequentially
+        for regpat in self.PATTERNS:
+            name = regpat.search(name)
+            if name is None:    # ignore the unexpected file
+                print('    find an invalid file: ', original_name)
+                return self.INF_VALUE
+            else:
+                name = name.group()
+
+        return int(name)
+
+    def __construct_dict(self):
+        files = os.listdir(self.DIR_PATH)
+        for f_name in files:
+            index = self.__get_index(f_name)
+            if index != self.INF_VALUE:
+                self.map[index] = f_name
+                self.valid += 1
+                self.max_index = max(self.max_index, index)
+            else:
+                self.invalid += 1
+
+    def work1(self, output = 'merged.pdf'):
+        print('Working...')
+        self.__construct_dict() # construct the map that from index to file name
+
+        pdf_writer = PdfFileWriter()
+
+        print('    Merging...')
+        # iterate every PDF file
+        # end = self.START_INDEX + self.valid - 1
+        end = self.max_index + 1
+        for i in range(self.START_INDEX, end):
+            try:    # support for the discontinuous index
+                f_name = self.map[i]
+            except KeyError as err_key: # when the key doesn't exists
+                print('Can\'t find key: ', err_key, ' , ignored.')
+                continue
+
+            file_path = self.DIR_PATH + '/' + f_name
+
+            pdf_reader = PdfFileReader(file_path)
+            # read every page
+            for page in range(pdf_reader.getNumPages()):
+                # add every page read into the writer
+                pdf_writer.addPage(pdf_reader.getPage(page))
+
+            print('    ', f_name, ' is merged.')
+            self.merged += 1
+
+        output_file = self.DIR_PATH + '/' + output
+        with open(output_file, 'wb') as outf:
+            pdf_writer.write(outf)  # output
+            print('Finished merging ', self.merged, ' files.')
+
+    def work2(self, output = 'merged.pdf'):
+        print('Working...')
+        self.__construct_dict()  # construct the map that from index to file name
+
+        pdf_merger = PdfFileMerger()
+
+        print('    Merging...')
+        # iterate every PDF file
+        # end = self.START_INDEX + self.valid - 1
+        end = self.max_index + 1
+        for i in range(self.START_INDEX, end):
+            try:
+                f_name = self.map[i]
+            except KeyError as err_key:
+                print('Can\'t find key: ', err_key, ' , ignored.')
+                continue
+            file_path = self.DIR_PATH + '/' + f_name
+            pdf_merger.append(file_path)
+            print('    ', f_name , ' is merged.')
+            self.merged += 1
+
+        output_file = self.DIR_PATH + '/' + output
+        pdf_merger.write(output_file)
+        print('Finished merging ', self.merged, ' files.')
+
+    def work3(self, output = 'merged.pdf'):
+        print('Working...')
+        self.__construct_dict() # construct the map that from index to file name
+
+        pdf_writer = PdfFileWriter()
+
+        print('    Merging...')
+        # end = self.START_INDEX + self.valid - 1
+        end = self.max_index + 1
+        for i in range(self.START_INDEX, end):
+            try:
+                f_name = self.map[i]
+            except KeyError as err_key:
+                print('Can\'t find key: ', err_key, ' , ignored.')
+                continue
+
+            file_path = self.DIR_PATH + '/' + f_name
+
+            pdf_reader = PdfFileReader(file_path)
+            pdf_writer.appendPagesFromReader(pdf_reader)
+            print('    ', f_name, ' is merged.')
+            self.merged += 1
+
+        output_file = self.DIR_PATH + '/' + output
+        with open(output_file, 'wb') as outf:
+            pdf_writer.write(outf)
+            print('Finished merging ', self.merged, ' files.')
+
+    def work(self, output = 'merged.pdf', work_type = 2):
+        start_time = time.time()
+
+        if work_type is 1:
+            self.work1(output)
+        elif work_type is 2:
+            self.work2(output)
+        elif work_type is 3:
+            self.work3(output)
+
+        end_time = time.time()
+        print('Merging completed in ', end_time - start_time, ' s')
+
+
+if __name__ == '__main__':
+    merger = PdfMerger()
+    merger.work('output.pdf', work_type = 3)
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Introduction
+
+PDF tools by PyPDF2.  
+
+# Details
+
+Visit my blog for details:  
+https://blog.valderfield.com/archives/19/  
+
+## PDF Merger Usage:  
+
+### Edit some values before run `pdfmerger.py` in `__init__` method:  
+在运行程序之前，先设定一些`__init__`方法中重要的变量：
+
+`self.DIR_PATH = ` your directory where PDF files exist (DON'T end with "/")（放PDF文件的目录地址。最后不要加斜杠！）  
+
+Fill the list of regular expressions:  
+填正则表达式列表（这些表达式会被依次执行，每次执行的结果作为下一次的输入）：
+`RE_LIST = [
+    
+]`
+
+You can fill `\d+` in `[` and `]` if you are lucky.  
+如果你的文件名中从头开始的第一个数字（连续数字如123视为一个数字）便是你想要的序号，那么你只需要在中括号里填入一项`\d+`即可。  
+
+The start index that can be matched of the PDF：  
+需要合并的PDF文件的文件名中起始的序号：  
+`self.START_INDEX = 1`
+
+
+A value greater than the number of PDFs:  
+一个大于需要合并的PDF文件总数的数值：
+`self.INF_VALUE = 100000  # usually don't need to change unless you have toooo many PDFs`
+
+You can change the codes in `__get_index` method for other(more complex) regular expression matching.  
+如果你想实现更复杂的正则匹配功能，你可以修改`__get_index`方法中的代码。  
+
+### Set the output file name and working type:
+设置输出文件名称和工作模式：  
+`merger.work('output.pdf', work_type = 3)`  
+
+The output file is `output.pdf` in the directory you set before(where your original PDF files exist).
+此时输出文件为`output.pdf`，放在原来存放PDF的文件夹里。  
+
+You can keep the `work_type` value.
+你可以不改变`work_type`的现有值。
